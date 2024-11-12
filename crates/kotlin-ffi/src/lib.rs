fd71555baa107a6eb4183215c3921cdf52cc1751
kubel <jakubwitczak6@gmail.com>
2024-11-12 14:56:26 +0100

    6492 verification

--- crates/kotlin-ffi/src/lib.rs
+++ crates/kotlin-ffi/src/lib.rs
@@ -1,5 +1,10 @@
 uniffi::setup_scaffolding!();
 
+use alloy::{
+    network::Ethereum,
+    primitives::{Address as YAddress, Bytes, B256},
+    providers::ReqwestProvider,
+};
 use yttrium::config::Config;
 use yttrium::transaction::send::safe_test::{
     Address, OwnerSignature as YOwnerSignature, Signature,
@@ -51,6 +56,61 @@ pub struct OwnerSignature {
 pub enum Error {
     #[error("Unknown {0}")]
     Unknown(String),
+}
+
+#[derive(uniffi::Object)]
+pub struct Erc6492Client {
+    provider: ReqwestProvider<Ethereum>,
+}
+
+#[derive(Debug, thiserror::Error, uniffi::Error)]
+pub enum Erc6492Error {
+    #[error("InvalidSignature")]
+    InvalidSignature(String),
+    #[error("InvalidAddress")]
+    InvalidAddress(String),
+    #[error("InvalidMessageHash")]
+    InvalidMessageHash(String),
+    #[error("Verification")]
+    Verification(String),
+}
+
+#[uniffi::export(async_runtime = "tokio")]
+impl Erc6492Client {
+    #[uniffi::constructor]
+    pub fn new(rpc_url: String) -> Self {
+        let url = rpc_url.parse().expect("Invalid RPC URL");
+        let provider = ReqwestProvider::<Ethereum>::new_http(url);
+        Self { provider }
+    }
+
+    pub async fn verify_signature(
+        &self,
+        signature: String,
+        address: String,
+        message_hash: String,
+    ) -> Result<bool, Erc6492Error> {
+        let signature = signature
+            .parse::<Bytes>()
+            .map_err(|e| Erc6492Error::InvalidSignature(e.to_string()))?;
+        let address = address
+            .parse::<YAddress>()
+            .map_err(|e| Erc6492Error::InvalidAddress(e.to_string()))?;
+        let message_hash = message_hash
+            .parse::<B256>()
+            .map_err(|e| Erc6492Error::InvalidMessageHash(e.to_string()))?;
+
+        let verification = erc6492::verify_signature(
+            signature,
+            address,
+            message_hash,
+            &self.provider,
+        )
+        .await
+        .map_err(|e| Erc6492Error::Verification(e.to_string()))?;
+
+        Ok(verification.is_valid())
+    }
 }
 
 #[uniffi::export(async_runtime = "tokio")]
