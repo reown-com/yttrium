71428c5bccb074b3d0d22b3f2527ce0597bc8936
Alfreedom <00tango.bromine@icloud.com>
2024-11-20 15:49:28 +0100

    Base project

---
+++ crates/dart-ffi/example/dart_ffi_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_ffi/dart_ffi.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ crates/dart-ffi/test/dart_ffi_test.dart
@@ -0,0 +1,16 @@
+import 'package:dart_ffi/dart_ffi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
