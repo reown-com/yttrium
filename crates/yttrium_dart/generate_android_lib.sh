fb2ba7c024275c6039947ef826b4723163bc0ab5
Alfreedom <alfredo@reown.com>
2024-12-11 17:45:16 +0100

    [Flutter] Setup script and modified workflow (#86)
    * Modified workflow to include ios release and created setup script for post install with flutter
    
    * Added script for manual generation and updated flutter_rust_bridge

---
+++ crates/yttrium_dart/generate_android_lib.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# run this script from inside /crates/yttrium_dart/
+
+cd rust
+
+rustup target add armv7-linux-androideabi aarch64-linux-android x86_64-linux-android i686-linux-android
+cargo ndk -t armeabi-v7a -t arm64-v8a -t x86_64 -t x86 build --release
+
+cd .. #/yttrium_dart
+
+mkdir -p android/src/main/jniLibs/arm64-v8a
+mkdir -p android/src/main/jniLibs/armeabi-v7a
+# mkdir -p android/src/main/jniLibs/x86
+# mkdir -p android/src/main/jniLibs/x86_64
+
+cd .. #/crates
+cd .. #/yttrium
+
+cp target/aarch64-linux-android/release/libyttrium_dart.so crates/yttrium_dart/android/src/main/jniLibs/arm64-v8a/libyttrium_dart.so
+cp target/armv7-linux-androideabi/release/libyttrium_dart.so crates/yttrium_dart/android/src/main/jniLibs/armeabi-v7a/libyttrium_dart.so
+# cp target/i686-linux-android/release/libyttrium_dart.so crates/yttrium_dart/android/src/main/jniLibs/x86/libyttrium_dart.so
+# cp target/x86_64-linux-android/release/libyttrium_dart.so crates/yttrium_dart/android/src/main/jniLibs/x86_64/libyttrium_dart.so
---
+++ crates/yttrium_dart/generate_ios_lib.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# run this script from inside /crates/yttrium_dart/
+
+DEFAULT_TARGET="aarch64-apple-ios"
+
+if [ $# -eq 0 ];
+then
+  echo "✅ $0: No arguments passed, building for target: $DEFAULT_TARGET"
+  TARGET=$DEFAULT_TARGET
+  # exit 1
+elif [ $# -gt 1 ];
+then
+  echo "❌ $0: Too many arguments, only one accepted. Arguments passed: $@"
+  exit 1
+else
+  if [ $1 != "-sim" ];
+  then
+    echo "❌ $0: Wrong argument $1 passed. Only valid argument is '-sim'"
+    exit 1
+  else
+    TARGET="aarch64-apple-ios$1"
+    echo "✅ $0: Building for targe: $TARGET"
+  fi
+fi
+
+cd rust
+
+echo "✅ $0: building for targest: x86_64-apple-ios $TARGET."
+
+rustup target add x86_64-apple-ios $TARGET
+
+cargo build --manifest-path Cargo.toml --target x86_64-apple-ios --release
+cargo build --manifest-path Cargo.toml --target $TARGET --release
+
+cd .. #/yttrium_dart
+cd .. #/crates
+cd .. #/yttrium
+
+mkdir -p target/universal/ios/release
+
+lipo -create target/$TARGET/release/libyttrium_dart.dylib target/x86_64-apple-ios/release/libyttrium_dart.dylib -output target/universal/ios/release/libyttrium_dart_universal.dylib
+
+lipo -info target/universal/ios/release/libyttrium_dart_universal.dylib
+
+cp target/universal/ios/release/libyttrium_dart_universal.dylib crates/yttrium_dart/ios/libyttrium_dart_universal.dylib
+
+# otool -L crates/yttrium_dart/ios/libyttrium_dart_universal.dylib
+
+install_name_tool -id @rpath/libyttrium_dart_universal.dylib crates/yttrium_dart/ios/libyttrium_dart_universal.dylib
+
+codesign --force --sign - crates/yttrium_dart/ios/libyttrium_dart_universal.dylib
+
+# otool -L crates/yttrium_dart/ios/libyttrium_dart_universal.dylib
+
+cd crates/yttrium_dart/example
+flutter clean
+flutter pub get
+
+cd ios
+pod deintegrate && rm Podfile.lock && pod cache clean -all && pod install
