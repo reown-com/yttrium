d9053c441404dc4b651f7bd6dd2c29b0dc88ac62
Chris Smith <1979423+chris13524@users.noreply.github.com>
2024-12-13 11:33:06 -0500

    feat: 7702 working (#90)
    * chore: improve docs
    
    * chore: use Blockchain API again for CA tests
    
    * chore: upgrade Safe launchpad to audited versions and 7702 progress
    
    * chore: working 7702

---
+++ crates/yttrium/src/erc7579/addresses.rs
@@ -0,0 +1,7 @@
+use alloy::primitives::{address, Address};
+
+// https://github.com/rhinestonewtf/module-sdk/blob/126de52a24fa2b3bad932b2ee8041675a9b766c2/src/module/registry/constants.ts#L3
+pub const RHINESTONE_ATTESTER_ADDRESS: Address =
+    address!("000000333034E9f539ce08819E12c1b8Cb29084d");
+pub const MOCK_ATTESTER_ADDRESS: Address =
+    address!("A4C777199658a41688E9488c4EcbD7a2925Cc23A");
