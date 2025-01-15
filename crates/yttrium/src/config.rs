f65d5fe1c8dc4fd33da07c2c1feb5522321a41f2
Gancho Radkov <ganchoradkov93@gmail.com>
2025-01-15 15:02:54 +0200

    chore: builds `Endpoint` to wasm

--- crates/yttrium/src/config.rs
+++ crates/yttrium/src/config.rs
@@ -1,26 +1,29 @@
 use {dotenvy::dotenv, std::env};
+use wasm_bindgen::prelude::*;
 
 pub const LOCAL_RPC_URL: &str = "http://localhost:8545";
 pub const LOCAL_BUNDLER_URL: &str = "http://localhost:4337";
 pub const LOCAL_PAYMASTER_URL: &str = "http://localhost:3000";
 
 #[derive(Clone, Debug, PartialEq)]
+#[wasm_bindgen(getter_with_clone)]
 #[cfg_attr(feature = "uniffi", derive(uniffi_macros::Record))]
 pub struct Config {
     pub endpoints: Endpoints,
 }
 
+#[wasm_bindgen]
 impl Config {
     pub fn local() -> Self {
         Config { endpoints: Endpoints::local() }
     }
-
     pub fn pimlico() -> Self {
         Config { endpoints: Endpoints::pimlico() }
     }
 }
 
 #[derive(Clone, Debug, PartialEq)]
+#[wasm_bindgen(getter_with_clone)]
 #[cfg_attr(feature = "uniffi", derive(uniffi_macros::Record))]
 pub struct Endpoints {
     pub rpc: Endpoint,
@@ -28,7 +31,14 @@ pub struct Endpoints {
     pub paymaster: Endpoint,
 }
 
+#[wasm_bindgen]
 impl Endpoints {
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(rpc: Endpoint, bundler: Endpoint, paymaster: Endpoint) -> Endpoints {
+        Endpoints { rpc, bundler, paymaster }
+    }
+
     pub fn live() -> Self {
         dotenv().unwrap();
 
@@ -80,13 +90,21 @@ impl Endpoints {
 }
 
 #[derive(Clone, Debug, PartialEq)]
+#[wasm_bindgen(getter_with_clone)]
 #[cfg_attr(feature = "uniffi", derive(uniffi_macros::Record))]
 pub struct Endpoint {
     pub base_url: String,
     pub api_key: String,
 }
 
+#[wasm_bindgen]
 impl Endpoint {
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(base_url: String, api_key: String) -> Endpoint {
+        Endpoint { base_url, api_key }
+    }
+
     pub fn local_rpc() -> Self {
         Endpoint {
             base_url: LOCAL_RPC_URL.to_string(),
@@ -100,7 +118,7 @@ impl Endpoint {
             api_key: "".to_string(),
         }
     }
-
+    
     pub fn local_paymaster() -> Self {
         Endpoint {
             base_url: LOCAL_PAYMASTER_URL.to_string(),
