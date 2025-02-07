cdb2f7dd9ecbca926c45e800d231de4c036373e3
Alfreedom <00tango.bromine@icloud.com>
2025-02-07 14:49:15 +0100

    Regenarete code after pull from main

--- crates/yttrium_dart/lib/generated/lib.freezed.dart
+++ crates/yttrium_dart/lib/generated/lib.freezed.dart
@@ -15,7 +15,7 @@ final _privateConstructorUsedError = UnsupportedError(
     'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
 /// @nodoc
-mixin _$FFIError {
+mixin _$Error {
   String get field0 => throw _privateConstructorUsedError;
   @optionalTypeArgs
   TResult when<TResult extends Object?>({
@@ -35,47 +35,46 @@ mixin _$FFIError {
       throw _privateConstructorUsedError;
   @optionalTypeArgs
   TResult map<TResult extends Object?>({
-    required TResult Function(FFIError_General value) general,
+    required TResult Function(Error_General value) general,
   }) =>
       throw _privateConstructorUsedError;
   @optionalTypeArgs
   TResult? mapOrNull<TResult extends Object?>({
-    TResult? Function(FFIError_General value)? general,
+    TResult? Function(Error_General value)? general,
   }) =>
       throw _privateConstructorUsedError;
   @optionalTypeArgs
   TResult maybeMap<TResult extends Object?>({
-    TResult Function(FFIError_General value)? general,
+    TResult Function(Error_General value)? general,
     required TResult orElse(),
   }) =>
       throw _privateConstructorUsedError;
 
-  /// Create a copy of FFIError
+  /// Create a copy of Error
   /// with the given fields replaced by the non-null parameter values.
   @JsonKey(includeFromJson: false, includeToJson: false)
-  $FFIErrorCopyWith<FFIError> get copyWith =>
-      throw _privateConstructorUsedError;
+  $ErrorCopyWith<Error> get copyWith => throw _privateConstructorUsedError;
 }
 
 /// @nodoc
-abstract class $FFIErrorCopyWith<$Res> {
-  factory $FFIErrorCopyWith(FFIError value, $Res Function(FFIError) then) =
-      _$FFIErrorCopyWithImpl<$Res, FFIError>;
+abstract class $ErrorCopyWith<$Res> {
+  factory $ErrorCopyWith(Error value, $Res Function(Error) then) =
+      _$ErrorCopyWithImpl<$Res, Error>;
   @useResult
   $Res call({String field0});
 }
 
 /// @nodoc
-class _$FFIErrorCopyWithImpl<$Res, $Val extends FFIError>
-    implements $FFIErrorCopyWith<$Res> {
-  _$FFIErrorCopyWithImpl(this._value, this._then);
+class _$ErrorCopyWithImpl<$Res, $Val extends Error>
+    implements $ErrorCopyWith<$Res> {
+  _$ErrorCopyWithImpl(this._value, this._then);
 
   // ignore: unused_field
   final $Val _value;
   // ignore: unused_field
   final $Res Function($Val) _then;
 
-  /// Create a copy of FFIError
+  /// Create a copy of Error
   /// with the given fields replaced by the non-null parameter values.
   @pragma('vm:prefer-inline')
   @override
@@ -92,32 +91,32 @@ class _$FFIErrorCopyWithImpl<$Res, $Val extends FFIError>
 }
 
 /// @nodoc
-abstract class _$$FFIError_GeneralImplCopyWith<$Res>
-    implements $FFIErrorCopyWith<$Res> {
-  factory _$$FFIError_GeneralImplCopyWith(_$FFIError_GeneralImpl value,
-          $Res Function(_$FFIError_GeneralImpl) then) =
-      __$$FFIError_GeneralImplCopyWithImpl<$Res>;
+abstract class _$$Error_GeneralImplCopyWith<$Res>
+    implements $ErrorCopyWith<$Res> {
+  factory _$$Error_GeneralImplCopyWith(
+          _$Error_GeneralImpl value, $Res Function(_$Error_GeneralImpl) then) =
+      __$$Error_GeneralImplCopyWithImpl<$Res>;
   @override
   @useResult
   $Res call({String field0});
 }
 
 /// @nodoc
-class __$$FFIError_GeneralImplCopyWithImpl<$Res>
-    extends _$FFIErrorCopyWithImpl<$Res, _$FFIError_GeneralImpl>
-    implements _$$FFIError_GeneralImplCopyWith<$Res> {
-  __$$FFIError_GeneralImplCopyWithImpl(_$FFIError_GeneralImpl _value,
-      $Res Function(_$FFIError_GeneralImpl) _then)
+class __$$Error_GeneralImplCopyWithImpl<$Res>
+    extends _$ErrorCopyWithImpl<$Res, _$Error_GeneralImpl>
+    implements _$$Error_GeneralImplCopyWith<$Res> {
+  __$$Error_GeneralImplCopyWithImpl(
+      _$Error_GeneralImpl _value, $Res Function(_$Error_GeneralImpl) _then)
       : super(_value, _then);
 
-  /// Create a copy of FFIError
+  /// Create a copy of Error
   /// with the given fields replaced by the non-null parameter values.
   @pragma('vm:prefer-inline')
   @override
   $Res call({
     Object? field0 = null,
   }) {
-    return _then(_$FFIError_GeneralImpl(
+    return _then(_$Error_GeneralImpl(
       null == field0
           ? _value.field0
           : field0 // ignore: cast_nullable_to_non_nullable
@@ -128,36 +127,35 @@ class __$$FFIError_GeneralImplCopyWithImpl<$Res>
 
 /// @nodoc
 
-class _$FFIError_GeneralImpl extends FFIError_General {
-  const _$FFIError_GeneralImpl(this.field0) : super._();
+class _$Error_GeneralImpl extends Error_General {
+  const _$Error_GeneralImpl(this.field0) : super._();
 
   @override
   final String field0;
 
   @override
   String toString() {
-    return 'FFIError.general(field0: $field0)';
+    return 'Error.general(field0: $field0)';
   }
 
   @override
   bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
-            other is _$FFIError_GeneralImpl &&
+            other is _$Error_GeneralImpl &&
             (identical(other.field0, field0) || other.field0 == field0));
   }
 
   @override
   int get hashCode => Object.hash(runtimeType, field0);
 
-  /// Create a copy of FFIError
+  /// Create a copy of Error
   /// with the given fields replaced by the non-null parameter values.
   @JsonKey(includeFromJson: false, includeToJson: false)
   @override
   @pragma('vm:prefer-inline')
-  _$$FFIError_GeneralImplCopyWith<_$FFIError_GeneralImpl> get copyWith =>
-      __$$FFIError_GeneralImplCopyWithImpl<_$FFIError_GeneralImpl>(
-          this, _$identity);
+  _$$Error_GeneralImplCopyWith<_$Error_GeneralImpl> get copyWith =>
+      __$$Error_GeneralImplCopyWithImpl<_$Error_GeneralImpl>(this, _$identity);
 
   @override
   @optionalTypeArgs
@@ -190,7 +188,7 @@ class _$FFIError_GeneralImpl extends FFIError_General {
   @override
   @optionalTypeArgs
   TResult map<TResult extends Object?>({
-    required TResult Function(FFIError_General value) general,
+    required TResult Function(Error_General value) general,
   }) {
     return general(this);
   }
@@ -198,7 +196,7 @@ class _$FFIError_GeneralImpl extends FFIError_General {
   @override
   @optionalTypeArgs
   TResult? mapOrNull<TResult extends Object?>({
-    TResult? Function(FFIError_General value)? general,
+    TResult? Function(Error_General value)? general,
   }) {
     return general?.call(this);
   }
@@ -206,7 +204,7 @@ class _$FFIError_GeneralImpl extends FFIError_General {
   @override
   @optionalTypeArgs
   TResult maybeMap<TResult extends Object?>({
-    TResult Function(FFIError_General value)? general,
+    TResult Function(Error_General value)? general,
     required TResult orElse(),
   }) {
     if (general != null) {
@@ -216,17 +214,17 @@ class _$FFIError_GeneralImpl extends FFIError_General {
   }
 }
 
-abstract class FFIError_General extends FFIError {
-  const factory FFIError_General(final String field0) = _$FFIError_GeneralImpl;
-  const FFIError_General._() : super._();
+abstract class Error_General extends Error {
+  const factory Error_General(final String field0) = _$Error_GeneralImpl;
+  const Error_General._() : super._();
 
   @override
   String get field0;
 
-  /// Create a copy of FFIError
+  /// Create a copy of Error
   /// with the given fields replaced by the non-null parameter values.
   @override
   @JsonKey(includeFromJson: false, includeToJson: false)
-  _$$FFIError_GeneralImplCopyWith<_$FFIError_GeneralImpl> get copyWith =>
+  _$$Error_GeneralImplCopyWith<_$Error_GeneralImpl> get copyWith =>
       throw _privateConstructorUsedError;
 }
