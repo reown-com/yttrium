54663d89c4fa517f6b7831b5f753e0502c74a8a7
llbartekll <bartus000@gmail.com>
2025-02-17 13:13:25 +0100

    make the shit work

--- Package.swift
+++ Package.swift
@@ -2,18 +2,6 @@
 import PackageDescription
 import Foundation
 
-let useLocalRustXcframework = ProcessInfo.processInfo.environment["USE_LOCAL_RUST_XCFRAMEWORK"] == "1"
-
-let yttriumXcframeworkTarget: Target = useLocalRustXcframework ?
-    .binaryTarget(
-        name: "YttriumXCFramework",
-        path: "target/ios/libuniffi_yttrium.xcframework"
-    ) :
-    .binaryTarget(
-        name: "YttriumXCFramework",
-        url: "https://github.com/reown-com/yttrium/releases/download/0.8.15/libuniffi_yttrium.xcframework.zip",
-        checksum: "aedae8bc4829fdebc2deab350c7dc626bda48336e60d9bef8b6f15ae8a877f7a"
-    )
 
 let package = Package(
     name: "Yttrium",
@@ -27,17 +15,18 @@ let package = Package(
         ),
     ],
     targets: [
-        yttriumXcframeworkTarget,
         .target(
             name: "Yttrium",
             dependencies: ["YttriumXCFramework"],
             path: "platforms/swift/Sources/Yttrium",
             publicHeadersPath: ".",
             cSettings: [
-                .headerSearchPath("."),
-                // Tells the compiler to use yttrium.modulemap instead of the default module.modulemap
-                .unsafeFlags(["-fmodule-map-file=yttrium.modulemap"])
+                .headerSearchPath(".")
             ]
+        ),
+        .binaryTarget(
+            name: "YttriumXCFramework",
+            path: "target/ios/libuniffi_yttrium.xcframework"
         )
     ]
 )
