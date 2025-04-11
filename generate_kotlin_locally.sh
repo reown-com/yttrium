33b9ca8d75ab976025e4e14fb421c48039ab381e
Chris Smith <chris@reown.com>
2025-04-11 00:49:03 +0000

    chore: use script

--- generate_kotlin_locally.sh
+++ generate_kotlin_locally.sh
@@ -18,7 +18,7 @@ mv target/armv7-linux-androideabi/uniffi-release-kotlin/libuniffi_yttrium.so cra
 mv yttrium/kotlin-bindings/uniffi/uniffi_yttrium/uniffi_yttrium.kt crates/kotlin-ffi/android/src/main/kotlin/com/reown/yttrium/
 mv yttrium/kotlin-bindings/uniffi/yttrium/yttrium.kt crates/kotlin-ffi/android/src/main/kotlin/com/reown/yttrium/
 
-$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/llvm-strip crates/kotlin-ffi/android/src/main/jniLibs/arm64-v8a/libuniffi_yttrium.so
-$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/llvm-strip crates/kotlin-ffi/android/src/main/jniLibs/armeabi-v7a/libuniffi_yttrium.so
+$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/*/bin/llvm-strip crates/kotlin-ffi/android/src/main/jniLibs/arm64-v8a/libuniffi_yttrium.so
+$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/*/bin/llvm-strip crates/kotlin-ffi/android/src/main/jniLibs/armeabi-v7a/libuniffi_yttrium.so
 
 gradle clean assembleRelease publishToMavenLocal
