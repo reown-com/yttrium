e63a80cf56c90f792818c3195f053c7e3ac668c6
Chris Smith <chris@reown.com>
2025-06-04 08:30:41 -0400

    fix: CI

--- generate_kotlin_locally.sh
+++ generate_kotlin_locally.sh
@@ -5,8 +5,8 @@ rm -rf crates/kotlin-ffi/android/src/main/jniLibs/arm64-v8a/
 rm -rf crates/kotlin-ffi/android/src/main/jniLibs/armeabi-v7a/
 rm -rf crates/kotlin-ffi/android/src/main/kotlin/com/reown/yttrium/
 
-cargo ndk -t armv7-linux-androideabi -t aarch64-linux-android build --profile=profile1 --features=android,uniffi/cli -p kotlin-ffi
-cargo run --features=android,uniffi/cli --bin uniffi-bindgen generate --library target/aarch64-linux-android/profile1/libuniffi_yttrium.so --language kotlin --out-dir yttrium/kotlin-bindings
+cargo ndk -t armv7-linux-androideabi -t aarch64-linux-android build --profile=profile1 --features=uniffi/cli -p kotlin-ffi
+cargo run --features=uniffi/cli --bin uniffi-bindgen generate --library target/aarch64-linux-android/profile1/libuniffi_yttrium.so --language kotlin --out-dir yttrium/kotlin-bindings
 
 mkdir -p crates/kotlin-ffi/android/src/main/jniLibs/arm64-v8a
 mkdir -p crates/kotlin-ffi/android/src/main/jniLibs/armeabi-v7a
