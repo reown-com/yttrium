1297b1ebf3c47c8f586549123f84f42b2c02ad01
jakubuid <jakubwitczak6@gmail.com>
2025-07-29 08:49:24 +0200

    revert

--- crates/yttrium/src/uniffi_compat/mod.rs
+++ crates/yttrium/src/uniffi_compat/mod.rs
@@ -21,6 +21,7 @@ use {
 };
 use {
     crate::{
+        chain_abstraction::api::prepare::Eip155OrSolanaAddress,
         smart_accounts::account_address::AccountAddress,
         wallet_service_api::{
             AddressOrNative, Asset, AssetData, Erc20Metadata, Erc721Metadata,
@@ -45,8 +46,6 @@ use {
     serde_json::Error as SerdeJsonError,
     uniffi::deps::anyhow::Error as AnyhowError,
 };
-#[cfg(feature = "chain_abstraction_client")]
-use crate::chain_abstraction::api::prepare::Eip155OrSolanaAddress;
 
 // TODO use https://mozilla.github.io/uniffi-rs/next/udl/remote_ext_types.html#remote-types when it's available
 
@@ -259,7 +258,6 @@ pub struct FfiAuthorization {
     pub nonce: u64,
 }
 
-#[cfg(feature = "chain_abstraction_client")]
 uniffi::custom_type!(Eip155OrSolanaAddress, String, {
     remote,
     try_lift: |val| Ok(val.parse()?),
