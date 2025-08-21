2c77af4b4ee8245cfbfc956a174fcad6b346779d
Chris Smith <chris@reown.com>
2025-08-21 09:16:37 -0400

    remove _json

--- crates/yttrium/src/sign/mod.rs
+++ crates/yttrium/src/sign/mod.rs
@@ -2038,18 +2038,6 @@ impl SignClient {
         Ok(proposal.into())
     }
 
-    pub async fn pair_json(&self, uri: String) -> Result<String, PairError> {
-        let proposal = {
-            let mut client = self.client.lock().await;
-            client.pair(&uri).await?
-        };
-        let proposal_ffi: SessionProposalFfi = proposal.into();
-        let serialized_proposal = serde_json::to_string(&proposal_ffi)
-            .expect("Failed to serialize response");
-
-        Ok(serialized_proposal)
-    }
-
     //TODO: Add approved namespaces builder util function
     pub async fn approve(
         &self,
@@ -2067,34 +2055,6 @@ impl SignClient {
             client.approve(proposal, approved_namespaces, self_metadata).await?
         };
         Ok(session.into())
-    }
-
-    pub async fn approve_json(
-        &self,
-        proposal: String,
-        approved_namespaces: String,
-        self_metadata: String,
-    ) -> Result<String, ApproveError> {
-        let proposal: SessionProposalFfi = serde_json::from_str(&proposal)
-            .expect("Failed to deserialize proposal");
-        let approved_namespaces: HashMap<String, SettleNamespace> =
-            serde_json::from_str(&approved_namespaces)
-                .expect("Failed to deserialize approved_namespaces");
-        let self_metadata: Metadata = serde_json::from_str(&self_metadata)
-            .expect("Failed to deserialize self_metadata");
-
-        tracing::debug!("approved_namespaces: {:?}", approved_namespaces);
-        tracing::debug!("self_metadata: {:?}", self_metadata);
-        let session = {
-            let mut client = self.client.lock().await;
-            client
-                .approve(proposal.into(), approved_namespaces, self_metadata)
-                .await?
-        };
-        let session_ffi: SessionFfi = session.into();
-        let serialized_session = serde_json::to_string(&session_ffi)
-            .expect("Failed to serialize response");
-        Ok(serialized_session)
     }
 
     pub async fn respond(
