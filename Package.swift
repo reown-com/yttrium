f59546502321cc337b4c9ea5415cfcea96f04d56
Bartosz Rozwarski <bartus000@gmail.com>
2025-08-28 08:40:01 +0300

    revert package change

--- Package.swift
+++ Package.swift
@@ -2,6 +2,19 @@
 import PackageDescription
 import Foundation
 
+let useLocalRustXcframework = ProcessInfo.processInfo.environment["USE_LOCAL_RUST_XCFRAMEWORK"] == "1"
+
+let yttriumXcframeworkTarget: Target = useLocalRustXcframework ?
+    .binaryTarget(
+        name: "YttriumXCFramework",
+        path: "target/ios/libyttrium.xcframework"
+    ) :
+    .binaryTarget(
+        name: "YttriumXCFramework",
+        url: "https://github.com/reown-com/yttrium/releases/download/0.9.57/libyttrium.xcframework.zip",
+        checksum: "3a2a36a251aafabb8f64780bff826d2088ce6a0a92a8934d7ac2f2b18667e2fa"
+    )
+
 let package = Package(
     name: "Yttrium",
     platforms: [
@@ -14,18 +27,15 @@ let package = Package(
         ),
     ],
     targets: [
+        yttriumXcframeworkTarget,
         .target(
             name: "Yttrium",
             dependencies: ["YttriumXCFramework"],
             path: "platforms/swift/Sources/Yttrium",
             publicHeadersPath: ".",
             cSettings: [
-                .headerSearchPath("."),
+                .headerSearchPath(".")
             ]
-        ),
-        .binaryTarget(
-            name: "YttriumXCFramework",
-            path: "target/ios/libyttrium.xcframework"
         )
     ]
 )
