2b5584b911056977829a579e2b3f64fb623c4870
Bartosz Rozwarski <bartus000@gmail.com>
2025-12-08 15:59:20 -0500

    savepoint

--- crates/pay-api/src/lib.rs
+++ crates/pay-api/src/lib.rs
@@ -1,11 +1,20 @@
 use serde::{Deserialize, Serialize};
 
 pub mod methods {
+    pub const CREATE_PAYMENT: &str = "createPayment";
     pub const GET_PAYMENT: &str = "getPayment";
 }
 
 pub mod currencies {
     pub const USD: &str = "iso4217/USD";
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct CreatePayment {
+    pub amount: String,
+    pub currency: String,
+    pub reference_id: String,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
