2fa905b50e25b35e80fdd45cdb7cf50599b32414
Bartosz Rozwarski <bartus000@gmail.com>
2025-12-09 17:18:20 -0500

    savepoint

--- crates/pay-api/src/lib.rs
+++ crates/pay-api/src/lib.rs
@@ -13,9 +13,9 @@ pub mod currencies {
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct CreatePayment {
-    pub amount: String,
-    pub currency: String,
-    pub reference_id: String,
+    pub amount: String,       // amount as a string in decimal (must support u256 for ERC-20)
+    pub currency: String,     // CAIP-19 or ISO 4217
+    pub reference_id: String, // 255 chars max custom reference ID
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
